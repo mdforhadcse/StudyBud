7849de8ff4a681cdefb2ea07ef31df6ff2e0f332
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-02-14 22:47:08 +0600

    learning template engine

--- base/views.py
+++ base/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Fronted Developer'},
+]
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return HttpResponse('Room')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
