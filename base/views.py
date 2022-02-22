a4dcbf1d978b8d6543166d0b389b632910aecc76
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-02-22 09:21:13 +0600

    User authentication and flash messages

--- base/views.py
+++ base/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -8,6 +12,33 @@ rooms = [
     {'id': 2, 'name': 'Design with me'},
     {'id': 3, 'name': 'Fronted Developer'},
 ]
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist!')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 # Create your views here.
@@ -20,7 +51,9 @@ def home(request):
     )
 
     topics = Topic.objects.all()
+
     room_count = rooms.count()
+
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
